f5502545b4bb7bd657adf9a39eec0ecf11d264d3
park cheolwoo <i15778645@gmail.com>
2019-11-07 14:36:28 +0900

    초기 커밋.

---
+++ MemoEngineCore/Demos/FrmHelloWorld.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MemoEngineCore
+{
+    public partial class FrmHelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClick_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = txtInput.Text;
+            txtInput.Text = String.Empty;
+        }
+    }
+}
